0848f6e2d028a71c8435cfc0f15fb3b92eb3fbad
David Minkovski <dminkovski@microsoft.com>
2025-01-16 10:42:29 +0100

    First Demo on Single Agent using Semantic Kernel

---
+++ BillInformation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalBankingAssistant
+{
+    public class BillInformation
+    {
+        public String billId;
+        public Double amount;
+        public String payeeName;
+
+        override
+        public String ToString()
+        {
+            return $"billId: {billId}, amount: {amount}, payeeName: {payeeName}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿#pragma warning disable SKEXP0110 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+#pragma warning disable SKEXP0001 
+
+// Copyright (c) Microsoft. All rights reserved.
+using System.ComponentModel;
+using Microsoft.SemanticKernel;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
+using Microsoft.Extensions.Configuration;
+
+namespace PersonalBankingAssistant
+{
+    public class Program
+    {
+        // Create Kernel based on appsettings values and add BankAssistant Functionality
+        private static Kernel buildKernel()
+        {
+            IConfiguration config = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.json")
+               .Build();
+
+            IKernelBuilder builder = Kernel.CreateBuilder();
+            builder.AddAzureOpenAIChatCompletion(
+                deploymentName: config["AzureOpenai:DeploymentName"],
+                endpoint: config["AzureOpenai:Endpoint"],
+                apiKey: config["AzureOpenai:ApiKey"]);
+
+            builder.Plugins.AddFromType<BankingAssistant>();
+
+            return builder.Build();
+        }
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Launching Personal Banking Assistant...");
+
+            Kernel kernel = buildKernel();
+
+            String systemPrompt = 
+                """
+                You are a home banking assistant, who allows users to pay their bill, by uploading a picture.
+                To pay a bill you need the filename of the image of the bill. Then you can scan the image using the filename.
+                Always check if a bill has already been paid before submitting a payment. All paid bills are part of the transaction history. 
+                Check the transaction history yourself.
+                Confirm the payment result.
+                """;
+
+            // Configure single agent "BankingAgent" with Auto Functionchoice behavior
+            ChatCompletionAgent agent = new()
+            {
+                Name = "BankingAgent",
+                Instructions = systemPrompt,
+                Kernel = kernel,
+                Arguments = new KernelArguments(
+                    new AzureOpenAIPromptExecutionSettings() { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() }
+                 )
+            };
+
+            Console.WriteLine("Welcome to your Personal Banking Assistant!");
+
+            ChatHistory history = [];
+
+            // Run main console interaction loop
+            bool isComplete = false;
+            do
+            {
+                Console.WriteLine();
+                Console.Write("> ");
+                string input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    continue;
+                }
+                if (input.Trim().Equals("EXIT", StringComparison.OrdinalIgnoreCase))
+                {
+                    isComplete = true;
+                    break;
+                }
+
+                history.Add(new ChatMessageContent(AuthorRole.User, input));
+
+                Console.WriteLine("");
+
+                // Output AI Agent response
+                await foreach (ChatMessageContent response in agent.InvokeAsync(history))
+                {
+                    Console.WriteLine($">> {response.Content}");
+                    history.Add(new ChatMessageContent(AuthorRole.System, response.Content));
+
+                }
+            } while (!isComplete);
+
+        }
+    }
+
+}
